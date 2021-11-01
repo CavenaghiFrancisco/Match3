981d7578503a3faf1028763717ea4a215a52af01
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-11-01 16:13:58 -0300

    Added grid script.

---
+++ Assets/Assets/Scripts/Grid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private int xSize;
+    [SerializeField] private int ySize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
