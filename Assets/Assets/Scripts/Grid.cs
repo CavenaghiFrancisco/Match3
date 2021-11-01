c6b92bae1b8cf113a5a6b6862d7648e079e2da90
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-11-01 16:59:59 -0300

    Added candy background texture and prefab.

--- Assets/Assets/Scripts/Grid.cs
+++ Assets/Assets/Scripts/Grid.cs
@@ -22,13 +22,30 @@ public class Grid : MonoBehaviour
     }
 
     public PiecePrefab[] piecePrefabs;
+    public GameObject backgroundPrefab;
 
     private Dictionary<PIECE_TYPE, GameObject> piecePrefabDict;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        piecePrefabDict = new Dictionary<PIECE_TYPE, GameObject>();
+        for(int i = 0; i < piecePrefabs.Length; i++)
+        {
+            if (!piecePrefabDict.ContainsKey(piecePrefabs[i].type))
+            {
+                piecePrefabDict.Add(piecePrefabs[i].type, piecePrefabs[i].prefab);
+            }
+        }
+
+        for(int x = 0; x < xSize; x++) 
+        {
+            for(int y = 0; y < ySize; y++)
+            {
+                GameObject background = (GameObject)Instantiate(backgroundPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                background.transform.parent = transform;
+            }
+        }
     }
 
     // Update is called once per frame
