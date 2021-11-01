b3520de4dac0dea157a7eb638f573fcf8e113f13
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-11-01 16:45:43 -0300

    Added candies prefabs.

--- Assets/Assets/Scripts/Grid.cs
+++ Assets/Assets/Scripts/Grid.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Grid : MonoBehaviour
 {
+    public enum PIECE_TYPE
+    {
+        NORMAL,
+        COUNT
+    }
+
     [SerializeField] private int xSize;
     [SerializeField] private int ySize;
+
+    [System.Serializable]
+    public struct PiecePrefab
+    {
+        public PIECE_TYPE type;
+        public GameObject prefab;
+    }
+
+    public PiecePrefab[] piecePrefabs;
+
+    private Dictionary<PIECE_TYPE, GameObject> piecePrefabDict;
 
     // Start is called before the first frame update
     void Start()
